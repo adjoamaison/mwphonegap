4a4bec6428231ac7f2d44219400ecbefb9e5fce7
adjoamaison <adjoa.maison@gmail.com>
2015-12-05 03:17:56 +0000

    phonegap for test

---
+++ js/login.js
@@ -0,0 +1,65 @@
+$.support.cors=true;
+
+$(document).ready(
+  validateLogin()
+);
+
+function validateLogin(){
+  if(localStorage.getItem("chef")!=null){
+    window.location.assign("recipeDashboard.html");
+  }
+}
+
+function sendRequest(u){
+    var obj=$.ajax({url:u,async:false});
+    var result=$.parseJSON(obj.responseText);
+    return result;  //return object
+}
+function login(){
+  var name = username.value;
+  var pword = inputPassword.value;
+  var strUrl = "../controller/response.php?cmd=16&name="+name+"&pword="+pword;
+  var objResult=sendRequest(strUrl);
+  if(objResult.chef!=false){
+    // alert(objResult.chef.chef_name);
+    localStorage.setItem("chef", objResult.chef.chef_name);
+    window.location.assign("recipeDashboard.html");
+  }
+}
+
+function addChef(){
+  var name = username.value;
+  var pword = inputPassword.value;
+  var num = number.value;
+  var ad = add.value;
+  var cemail = email.value;
+  var strUrl = "../controller/response.php?cmd=5&name="+name+"&pword="+pword+"&number="+num+"&address="+ad+"&email="+cemail;
+  var objResult=sendRequest(strUrl);
+  if(objResult.result==1){
+    localStorage.setItem("chef", name);
+    window.location.assign("recipeDashboard.html");
+  }
+}
+
+function chefSignUpForm(){
+  var form ="";
+  form += "<div class='container' ><div class='row'><div class='well well-lg'><form class='form-horizontal'>";
+  form += "<fieldset><legend><em>Cuisine Chef Sign up</em></legend><div class='form-group'>";
+  form += "<label for='username' class='col-lg-2 control-label'>Username</label><div class='col-lg-10'>";
+  form += "<input type='text' class='form-control' id='username' placeholder='username'></div></div>";
+  form += "<div class='form-group'><label for='inputPassword' class='col-lg-2 control-label'>Password</label>";
+  form += "<div class='col-lg-10'><input type='password' class='form-control' id='inputPassword' placeholder='Password'>";
+  form += "</div></div><div class='form-group'><label for='number' class='col-lg-2 control-label'>Phone Number</label>";
+  form += "<div class='col-lg-10'><input type='number' class='form-control' id='number' placeholder='233123456789'>";
+  form += "</div></div>";
+  form += "<div class='form-group'><label for='add' class='col-lg-2 control-label'>Address</label>";
+  form += "<div class='col-lg-10'><input type='text' class='form-control' id='add' placeholder='location'>";
+  form += "</div></div>";
+  form += "<div class='form-group'><label for='email' class='col-lg-2 control-label'>Email</label>";
+  form += "<div class='col-lg-10'><input type='text' class='form-control' id='email' placeholder='email'>";
+  form += "</div></div>";
+  form += "<div class='col-lg-10 col-lg-offset-2'><a href='#' onclick='addChef()' class='btn btn-default btn-lg btn-block'>Sign up</a>";
+  form += "</div></fieldset></form></div></div></div>";
+
+  $('#contain').html(form);
+}
---
+++ js/script.js
@@ -0,0 +1,208 @@
+$.support.cors=true;
+
+$(document).ready(
+  validateLogin()
+);
+
+function validateLogin(){
+  if(localStorage.getItem("chef")==null){
+    window.location.assign("index.html");
+  }
+}
+
+function logout(){
+  localStorage.removeItem("chef");
+  window.location.assign("index.html");
+}
+
+
+function sendRequest(u){
+    var obj=$.ajax({url:u,async:false});
+    var result=$.parseJSON(obj.responseText);
+    return result;  //return object
+}
+
+function recipeForm(){
+  $("#form").attr("class", "active");
+	$("#view").attr("class", "");
+  var form="";
+  form += "<form class='form-horizontal'><fieldset><legend>Add New Recipe</legend>";
+
+  form += "<div class='form-group'><label for='mealname' class='col-lg-2 control-label'>Recipe Name</label>";
+  form += "<div class='col-lg-10'><input type='text' class='form-control' id='mealname' placeholder='Name'>";
+  form += "</div></div>";
+
+  form += "<div class='form-group'><label for='overview' class='col-lg-2 control-label'>Overview</label>";
+  form += "<div class='col-lg-10'><textarea class='form-control' rows='3' id='overview'></textarea></div></div>";
+
+  form += "<div class='form-group'><label for='ingr' class='col-lg-2 control-label'>Ingredients</label>";
+  form += "<div class='col-lg-10'><textarea class='form-control' rows='3' id='ingr'></textarea></div></div>";
+
+  form += "<div class='form-group'><label for='instruction0' class='col-lg-2 control-label'>Instruction</label>";
+  form += "<div class='col-lg-10'><a href='#' onclick='addInst()' class='btn btn-primary btn-sm'>add</a>";
+  form += "<div id='inst'><input type='text' class='form-control' id='instruction0' placeholder='Instruction'></div>";
+  form += "</div></div>";
+
+  form += "<div class='form-group'><label for='cat' class='col-lg-2 control-label'>Category</label>";
+  form += "<div class='col-lg-10'><input type='text' class='form-control' id='cat' placeholder='Category'>";
+  form += "<br></div>";
+
+  form += "<div class='form-group'><label for='input-ids' class='col-lg-2 control-label'>Image</label>";
+  form += "<div class='col-lg-10'><a href='#' onclick='addImage()' class='btn btn-primary'>Select Image</a>";
+  form += "</div></div>";//image
+  form += "<div class='form-group'><div class='col-lg-10 col-lg-offset-2'>";
+  form += "<button type='reset' onclick='viewRecipe()' class='btn btn-default'>Cancel</button>";
+  form += "<input type='button' onclick='addRecipe()' class='btn btn-primary' value='Submit'></div></div>";
+
+  form += "</fieldset></form>";
+
+  $("#content").html(form);
+}
+  var count = 1;
+function addInst(){
+  var form = "";
+
+  form += "<input type='text' class='form-control' id='instruction"+count+"' placeholder='Instruction'>";
+  $("#inst").append(form);
+  count++;
+}
+
+function displayUname(){
+    $("#uname").html(localStorage.getItem("chef"));
+}
+
+function recipeDetails(id){
+  var strUrl = "../controller/response.php?cmd=3&id="+id;
+		var objResult=sendRequest(strUrl);
+		if(objResult.result==1){
+      var recipe="";
+      recipe += "<legend>"+objResult.recipe[0].mealname+"</legend><br>";
+      recipe += "<div class='panel panel-default'><div class='panel-heading'>OVERVIEW</div>";
+      recipe += "<div class='panel-body'>"+objResult.recipe[0].overview+"</div></div>";
+
+      recipe += "<div class='panel panel-default'><div class='panel-heading'>INGREDIENTS</div>";
+      recipe += "<div class='panel panel-default'><div class='panel-body'>";
+      recipe += objResult.recipe[0].ingredients+"</div></div></div>";
+
+      strUrl = "../controller/response.php?cmd=13&id="+id;
+    		var objResult=sendRequest(strUrl);
+    		if(objResult.result==1){
+          recipe += "<div class='panel panel-default'><div class='panel-heading'>INSTRUCTIONS</div>";
+          for (var i = 0; i < objResult.instructions.length; i++) {
+            recipe += "<div class='panel panel-default'><div class='panel-body'><b>";
+            recipe += (i+1) +".</b>  "+objResult.instructions[i].instruction+"</div></div>";
+          }
+          recipe += "</div>";
+        }
+      $("#content").html(recipe);
+    }else{
+			msg = "";
+	    msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    msg += "<span aria-hidden='true'>&times;</span></button>";
+	    msg += "No Details to Show.</div>";
+      $("#alert").html(msg);
+		}
+}
+
+
+
+
+//view recipe
+function viewRecipe(){
+  $("#view").attr("class", "active");
+  $("#form").attr("class", "");
+	var msg="";
+	var strUrl = "../controller/response.php?cmd=2";
+		var objResult=sendRequest(strUrl);
+		if(objResult.result==1){
+			var list ="";
+	    for ( var i = 0; i<objResult.recipes.length; i++) {
+  			list += "<div class='col-md-4'><div class='card'><div class='card-image' style='cursor: pointer' onclick=recipeDetails('"+objResult.recipes[i].recipe_id+"')>";
+        list += "<img width='100%' class='img-responsive' src='"+objResult.recipes[i].meal_image +"'>";
+        list += "</div>";
+        list += "<div class='card-content' style='cursor: pointer'onclick=recipeDetails('"+objResult.recipes[i].recipe_id+"')>";
+        list += "<span class='card-title'>"+objResult.recipes[i].mealname+"</span>";
+        list += "</div></div></div>";
+      }
+	    $("#content").html(list);
+    }else{
+			msg = "";
+	    msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    msg += "<span aria-hidden='true'>&times;</span></button>";
+	    msg += "No Recipes available.</div>";
+      $("#alert").html(msg);
+		}
+}
+
+function makeid(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function addRecipe(){
+  alert("addRece");
+  var id = makeid();
+  var name=mealname.value;
+  var over=overview.value;
+  var ingredient=ingr.value;
+  var inst=instruction;
+  var instruction =[];
+  var chef=localStorage.getItem("chef");
+  var c=cat.value;
+  var img = "img/meal.jpg";
+
+  // instruction.length=count;
+  for (var i = 0; i < count; i++) {
+    testd = "instruction";
+    var countd = testd.concat(i);
+    instruction[i]= document.getElementById(countd).value;
+    alert("instruction..." +instruction[i]);
+    var strUrl = "../controller/response.php?cmd=12&recipe="+id+"&instruction="+instruction[i];
+  		var objResult=sendRequest(strUrl);
+  }
+  var msg="";
+  alert(id+" ---id");
+  alert(name+" ---name");
+  alert(over+" ---view");
+  alert(chef+" ---chef");
+  alert(c+" ---cat");
+	var strUrl = "../controller/response.php?cmd=1&id="+id+"&name="+name+"&chef="+chef+"&overview="+over+"&ingr="+ingredient+"&image="+img+"&cat="+c;
+		var objResult=sendRequest(strUrl);
+		if(objResult.result==1){
+      msg = "";
+      msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+      msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+      msg += "<span aria-hidden='true'>&times;</span></button>";
+      msg += "Recipe succefully added.</div>";
+
+    }else{
+			msg = "";
+	    msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    msg += "<span aria-hidden='true'>&times;</span></button>";
+	    msg += "Could not add recipe.</div>";
+		}
+    $("#alert").html(msg);
+}
+
+
+function addImage(){
+  navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+}
+
+    function onSuccess(imageURI) {
+      var image = document.getElementById('myImage');
+      image.src = imageURI;
+    }
+
+  function onFail(message) {
+      alert('Failed because: ' + message);
+  }
